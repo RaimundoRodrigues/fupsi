a7074da343d6e907041f83af8b9215fa34519e2f
tux <sena.ufc@gmail.com>
2014-03-17 00:54:01 -0300

    update listas

---
+++ missoes/00_operacoes_tipos/01_constantes_faceis.cpp
@@ -0,0 +1,48 @@
+
+/*#################################################################*/
+/*###############            FÁCEIS                 ###############*/
+/*#################################################################*/
+
+/* Faça um programa que receba três notas e calcule a média dessas notas.
+*/
+float media_pond( float nota1, float nota2, float nota3 );
+
+/* 
+Faça um programa que receba três notas e seus respectivos pesos, 
+calcule e mostre a média ponderada dessas notas.
+*/
+float media_pond( float nota1, float nota2, float nota3, float peso1, float peso2, float peso3 );
+
+
+/* 
+Faça um programa que calcula o quadrado de um número dado como entrada.
+*/
+int quadrado(int valor);
+
+/*#################################################################*/
+/*###############            MÉDIAS                 ###############*/
+/*#################################################################*/
+
+/* 
+Faça um programa que retorna o IMC (índice da massa corpórea) de uma
+pessoa. O IMC é calculado com o peso em kg, dividido pelo quadrado da altura,
+em metros, IMC = Peso/Altura².
+*/
+float imc (float peso, float altura);
+
+/*
+Faça um programa que transforma uma temperatura em Celsius para uma
+temperatura em Fahrenheit. Use a seguinte regra de transformação:
+°C = (°F - 32) / 1,8
+*/
+float celsius_fahrenheit (float temperatura);
+
+
+/* 
+Faça duas funções, que determinem a partir da larguda, altura e comprimento de uma caixa d'água,
+(a) O volume de água que a caixa pode armazenar.
+(b) A área de material utilizada para construir a caixa com tampa.
+*/
+float calcula_volume ( float largura, float altura, float comprimento );
+float calcula_area ( float largura, float altura, float comprimento );
+
